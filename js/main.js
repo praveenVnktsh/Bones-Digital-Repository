349af61f9450a18b6a25c02b4d577e3146129ef2
awesomeroks <30774147+praveenVnktsh@users.noreply.github.com>
2022-01-18 20:22:22 +0530

    texture issues

--- js/main.js
+++ js/main.js
@@ -1,199 +1,200 @@
-var rotate = false; var cameraZ;
-            
-            if ( WEBGL.isWebGLAvailable() === false ) {
+var rotate = false;
+var cameraZ;
 
-                document.body.appendChild( WEBGL.getWebGLErrorMessage() );
+if (WEBGL.isWebGLAvailable() === false) {
+  document.body.appendChild(WEBGL.getWebGLErrorMessage());
+}
+function toggle_rotation() {
+  rotate = !rotate;
+}
 
-            }
-            function toggle_rotation () {
-                rotate = ! rotate;
-            }
-            
-            function zoom_refit (offset) {
-                offset =  offset || 1.15;
-                r=cameraZ*offset;
-                camera.position.x=r*Math.cos(2);
-                camera.position.z=r*Math.sin(2);
-                camera.lookAt(scene.position);
-                renderer.render( scene, camera );
-            }
-            
-            function fitCameraToObject( camera, object, offset, controls ) {
+function zoom_refit(offset) {
+  offset = offset || 1.15;
+  r = cameraZ * offset;
+  camera.position.x = r * Math.cos(2);
+  camera.position.z = r * Math.sin(2);
+  camera.lookAt(scene.position);
+  renderer.render(scene, camera);
+}
 
-                offset =  offset || 1.35;
-            
-                const boundingBox = new THREE.Box3();
-            
-                // get bounding box of object - this will be used to setup controls and camera
-                boundingBox.setFromObject( object );
-                    
-                        //ERRORS HERE
-                const center = boundingBox.getCenter();
-                const size = boundingBox.getSize();
-            
-                // get the max side of the bounding box (fits to width OR height as needed )
-                const maxDim = Math.max( size.x, size.y, size.z );
-                const fov = camera.fov * ( Math.PI / 180 );
-                cameraZ = Math.abs( maxDim / 2 * Math.tan( fov * 2 ) ); //Applied fifonik correction
-            
-                cameraZ *= offset; // zoom out a little so that objects don't fill the screen
-            
-                // <--- NEW CODE
-                //Method 1 to get object's world position
-                scene.updateMatrixWorld(); //Update world positions
-                var objectWorldPosition = new THREE.Vector3();
-                objectWorldPosition.setFromMatrixPosition( object.matrixWorld );
-                
-                //Method 2 to get object's world position
-                //objectWorldPosition = object.getWorldPosition();
-            
-                const directionVector = camera.position.sub(objectWorldPosition);   //Get vector from camera to object
-                const unitDirectionVector = directionVector.normalize(); // Convert to unit vector
-                camera.position = unitDirectionVector.multiplyScalar(cameraZ); //Multiply unit vector times cameraZ distance
-                camera.lookAt(objectWorldPosition); //Look at object
-                // --->
-            
-                const minZ = boundingBox.min.z;
-                const cameraToFarEdge = ( minZ < 0 ) ? -minZ + cameraZ : cameraZ - minZ;
-            
-                camera.far = cameraToFarEdge * 3;
-                camera.updateProjectionMatrix();
-            
-                if ( controls ) {
-            
-                // set camera to rotate around center of loaded object
-                controls.target = center;
-            
-                // prevent camera from zooming out far enough to create far plane cutoff
-                controls.maxDistance = cameraToFarEdge * 2;
-                        // ERROR HERE   
-                controls.saveState();
-            
-                } else {
-            
-                    camera.lookAt( center )
-            
-                }
-            }
-            
-            
-            
-            var container; //var container, stats;
+function fitCameraToObject(camera, object, offset, controls) {
+  offset = offset || 1.35;
 
-            var camera, controls, scene, renderer;
+  const boundingBox = new THREE.Box3();
 
-            init();
-            animate();
+  // get bounding box of object - this will be used to setup controls and camera
+  boundingBox.setFromObject(object);
 
-            function init() {
+  //ERRORS HERE
+  const center = boundingBox.getCenter();
+  const size = boundingBox.getSize();
 
-                camera = new THREE.PerspectiveCamera( 60, window.innerWidth / window.innerHeight, 0.01, 1e10 );
-                //camera = new THREE.PerspectiveCamera( 40, window.innerWidth / window.innerHeight, 1, 2000 );
-                camera.position.z = 6;
+  // get the max side of the bounding box (fits to width OR height as needed )
+  const maxDim = Math.max(size.x, size.y, size.z);
+  const fov = camera.fov * (Math.PI / 180);
+  cameraZ = Math.abs((maxDim / 2) * Math.tan(fov * 2)); //Applied fifonik correction
 
-                controls = new THREE.OrbitControls( camera );
+  cameraZ *= offset; // zoom out a little so that objects don't fill the screen
 
-                scene = new THREE.Scene();
-                scene.add( camera );
+  // <--- NEW CODE
+  //Method 1 to get object's world position
+  scene.updateMatrixWorld(); //Update world positions
+  var objectWorldPosition = new THREE.Vector3();
+  objectWorldPosition.setFromMatrixPosition(object.matrixWorld);
 
-                // light
+  //Method 2 to get object's world position
+  //objectWorldPosition = object.getWorldPosition();
 
-                var dirLight = new THREE.DirectionalLight( 0xffffff );
-                dirLight.position.set( 200, 200, 1000 ).normalize();
+  const directionVector = camera.position.sub(objectWorldPosition); //Get vector from camera to object
+  const unitDirectionVector = directionVector.normalize(); // Convert to unit vector
+  camera.position = unitDirectionVector.multiplyScalar(cameraZ); //Multiply unit vector times cameraZ distance
+  camera.lookAt(objectWorldPosition); //Look at object
+  // --->
 
-                camera.add( dirLight );
-                camera.add( dirLight.target );
+  const minZ = boundingBox.min.z;
+  const cameraToFarEdge = minZ < 0 ? -minZ + cameraZ : cameraZ - minZ;
 
-                // model
+  camera.far = cameraToFarEdge * 3;
+  camera.updateProjectionMatrix();
 
-				var onProgress = function ( xhr ) {
+  if (controls) {
+    // set camera to rotate around center of loaded object
+    controls.target = center;
 
-					if ( xhr.lengthComputable ) {
+    // prevent camera from zooming out far enough to create far plane cutoff
+    controls.maxDistance = cameraToFarEdge * 2;
+    // ERROR HERE
+    controls.saveState();
+  } else {
+    camera.lookAt(center);
+  }
+}
 
-						var percentComplete = xhr.loaded / xhr.total * 100;
-						console.log( Math.round( percentComplete, 2 ) + '% downloaded' );
+var container; //var container, stats;
 
-					}
+var camera, controls, scene, renderer;
 
-				};
-                
-				var onError = function () { };
+init();
+animate();
 
-				THREE.Loader.Handlers.add( /\.dds$/i, new THREE.DDSLoader() );
+function init() {
+  camera = new THREE.PerspectiveCamera(
+    60,
+    window.innerWidth / window.innerHeight,
+    0.01,
+    1e10
+  );
+  //camera = new THREE.PerspectiveCamera( 40, window.innerWidth / window.innerHeight, 1, 2000 );
+  camera.position.z = 6;
 
-				new THREE.MTLLoader()
-					.setPath( 'assets/' )
-					.load( 'icezum.mtl', function ( materials ) {
+  controls = new THREE.OrbitControls(camera);
 
-						materials.preload();
+  scene = new THREE.Scene();
+  scene.add(camera);
 
-						new THREE.OBJLoader()
-							.setMaterials( materials )
-							.setPath( 'assets/' )
-							.load( 'icezum.obj', function ( object ) {
+  // light
 
-								// object.position.y = - 05;
-								scene.add( object );
-                                fitCameraToObject ( camera, object, 1.15, controls );
+  var dirLight = new THREE.DirectionalLight(0xffffff);
+  dirLight.position.set(200, 200, 1000).normalize();
 
-							}, onProgress, onError );
+  camera.add(dirLight);
+  camera.add(dirLight.target);
 
-					} );
+  const loader = new THREE.GLTFLoader();
 
-				//
+  loader.load(
+    // resource URL
+    "assets/skull.glb",
+    // called when the resource is loaded
+    function (gltf) {
+      scene.add(gltf.scene);
 
-				
-                // var loader = new THREE.VRMLLoader();
-                // // loader.load( 'assets/house.wrl', function ( object ) {
-                // 
-                // loader.load( 'assets/icezum.wrl', function ( object ) {
-                // //loader.load( 'assets/oshwi.wrl', function ( object ) {
-                //     scene.add( object );
-                //     fitCameraToObject ( camera, object, 1.35, controls );
-                // 
-                // } );
+      gltf.animations; // Array<THREE.AnimationClip>
+      gltf.scene; // THREE.Group
+      gltf.scenes; // Array<THREE.Group>
+      gltf.cameras; // Array<THREE.Camera>
+      gltf.asset; // Object
+      
+    },
+    // called while loading is progressing
+    function (xhr) {
+      console.log((xhr.loaded / xhr.total) * 100 + "% loaded");
+    },
+    // called when loading has errors
+    function (error) {
+      console.log("An error happened");
+    }
+  );
+  //   THREE.Loader.Handlers.add(/\.dds$/i, new THREE.DDSLoader());
+  //   var modelName = "Head, Cranial";
+  //   new THREE.MTLLoader()
+  //     .setPath("assets/")
+  //     .load(modelName + ".mtl", function (materials) {
+  //       materials.preload();
 
-                // renderer
+  //       new THREE.OBJLoader()
+  //         .setMaterials(materials)
+  //         .setPath("assets/")
+  //         .load(
+  //           modelName + ".obj",
+  //           function (object) {
+  //             // object.position.y = - 05;
+  //             scene.add(object);
+  //             fitCameraToObject(camera, object, 1.15, controls);
+  //           },
+  //           onProgress,
+  //           onError
+  //         );
+  //     });
 
-                renderer = new THREE.WebGLRenderer();
-                renderer.setPixelRatio( window.devicePixelRatio );
-                renderer.setSize( window.innerWidth, window.innerHeight );
+  //
 
-                container = document.createElement( 'div' );
-                document.body.appendChild( container );
-                container.appendChild( renderer.domElement );
+  // var loader = new THREE.VRMLLoader();
+  // // loader.load( 'assets/house.wrl', function ( object ) {
+  //
+  // loader.load( 'assets/icezum.wrl', function ( object ) {
+  // //loader.load( 'assets/oshwi.wrl', function ( object ) {
+  //     scene.add( object );
+  //     fitCameraToObject ( camera, object, 1.35, controls );
+  //
+  // } );
 
-                // stats = new Stats();
-                // container.appendChild( stats.dom );
+  // renderer
 
-                //
+  renderer = new THREE.WebGLRenderer();
+  renderer.setPixelRatio(window.devicePixelRatio);
+  renderer.setSize(window.innerWidth, window.innerHeight);
 
-                window.addEventListener( 'resize', onWindowResize, false );
-                
-            }
+  container = document.createElement("div");
+  document.body.appendChild(container);
+  container.appendChild(renderer.domElement);
 
-            function onWindowResize() {
+  // stats = new Stats();
+  // container.appendChild( stats.dom );
 
-                camera.aspect = window.innerWidth / window.innerHeight;
-                camera.updateProjectionMatrix();
+  //
 
-                renderer.setSize( window.innerWidth, window.innerHeight );
+  window.addEventListener("resize", onWindowResize, false);
+}
 
-            }
+function onWindowResize() {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
 
-            function animate() {
+  renderer.setSize(window.innerWidth, window.innerHeight);
+}
 
-                requestAnimationFrame( animate );
+function animate() {
+  requestAnimationFrame(animate);
 
-                if (rotate) {
-                    var timer=Date.now() * 0.0005;
-                    r=cameraZ*1.25;
-                    camera.position.x=r*Math.cos(timer);
-                    camera.position.z=r*Math.sin(timer);
-                    camera.lookAt(scene.position);
-                    }
-                renderer.render( scene, camera );
+  if (rotate) {
+    var timer = Date.now() * 0.0005;
+    r = cameraZ * 1.25;
+    camera.position.x = r * Math.cos(timer);
+    camera.position.z = r * Math.sin(timer);
+    camera.lookAt(scene.position);
+  }
+  renderer.render(scene, camera);
 
-                // stats.update();
-
-            }
+  // stats.update();
+}
