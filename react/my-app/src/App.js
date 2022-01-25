67feefd01dbd39be0c73964089f579cc2dff3569
awesomeroks <30774147+praveenVnktsh@users.noreply.github.com>
2022-01-25 23:31:16 +0530

    built

--- react/my-app/src/App.js
+++ react/my-app/src/App.js
@@ -1,4 +1,5 @@
 import "./styles.css";
+import React, { Component }  from 'react';
 import { Canvas } from "@react-three/fiber";
 import { useLoader } from "@react-three/fiber";
 import { Environment, OrbitControls } from "@react-three/drei";
@@ -9,19 +10,46 @@ const Model = () => {
   const gltf = useLoader(GLTFLoader, "./Cranial.glb");
   return (
     <>
-      <primitive object={gltf.scene} scale={0.4} />
+      <primitive object={gltf.scene} scale={0.1} />
     </>
   );
 };
+// Geometry
+function GroundPlane() {
+  return (
+    <mesh receiveShadow rotation={[5, 0, 0]} position={[0, -1, 0]}>
+      <planeBufferGeometry attach="geometry" args={[500, 500]} />
+      <meshStandardMaterial attach="material" color="white" />
+    </mesh>
+  );
+}
 
+
+function RimLight({ brightness, color }) {
+  return (
+    <rectAreaLight
+      width={2}
+      height={2}
+      intensity={brightness}
+      color={color}
+      position={[1, 4, -2]}
+      rotation={[0, 180, 0]}
+      castShadow
+    />
+  );
+}
 export default function App() {
   return (
     <div className="App">
       <Canvas>
+      <ambientLight intensity={1} />
+      <spotLight intensity={0.5} angle={0.1} penumbra={1} position={[10, 15, 10]} castShadow />
+
         <Suspense fallback={null}>
           <Model />
           <OrbitControls />
-          <Environment preset="sunset"  />
+          <GroundPlane />
+
         </Suspense>
       </Canvas>
     </div>
